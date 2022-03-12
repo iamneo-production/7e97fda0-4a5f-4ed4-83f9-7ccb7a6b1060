aa29b0856a6fd3aa6f69b9691c9fa905d3a68e3d
venu630 <sirisanagandlavenu@gmail.com>
2022-03-12 17:48:16 +0000

    adding product controllers

--- springapp/src/main/java/com/examly/springapp/service/ProductService.java
+++ springapp/src/main/java/com/examly/springapp/service/ProductService.java
@@ -1,5 +1,30 @@
 package com.examly.springapp.service;
 
+import java.util.*;
+import com.examly.springapp.models.Product;
+import com.examly.springapp.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+
+        List<Product> products = new ArrayList<>();
+		productRepository.findAll()
+		.forEach(products :: add);
+		
+		return products;
+    }
+
+    public void productSave(Product product) {
+        productRepository.save(product);
+    }
+
+
     
 }
