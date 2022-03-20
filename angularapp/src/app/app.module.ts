5c6c9e17fe5796678aea9a6e4c1dc10a4a6db72d
Shivani-2121 <saishivani2121k@gmail.com>
2022-03-20 11:15:15 +0000

    Login page

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
