59c1322facc06c344ac75f8c09aeb463bc9d8a3a
venu630 <sirisanagandlavenu@gmail.com>
2022-03-29 15:37:54 +0000

    admin dashboard

--- angularapp/src/app/admin/dashboard/dashboard.component.ts
+++ angularapp/src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HomeService } from '../../services/Home/home.service';
+import {ModelService} from '../../services/Model/model.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  productDetails:any;
+  AllproductDetails = [{
+    productId:'',
+    imageUrl:'',
+    productName:'',
+    price:'',
+    description:'',
+    quantity:''
+  }]
+  id = '';
+  constructor(private http: HttpClient, private router: Router, private userData: HomeService, private productData: ModelService) { }
 
   ngOnInit(): void {
+    this.userData.getProductDetails().subscribe((data: any) =>{
+      this.productDetails = data;
+      console.log(this.productDetails);
+      this.AllproductDetails = this.productDetails;
+    })
   }
 
 }
