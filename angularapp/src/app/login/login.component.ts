81003fb4f97250efc6d8ffd574e5131f9cdc5410
venu630 <sirisanagandlavenu@gmail.com>
2022-04-04 16:50:35 +0000

    navbars modification

--- angularapp/src/app/login/login.component.ts
+++ angularapp/src/app/login/login.component.ts
@@ -53,6 +53,7 @@ export class LoginComponent implements OnInit {
       if(result==true)
       {
         localStorage.setItem('email', this.user.email);
+        localStorage.setItem('token',"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
         this.router.navigate(['home']);
       }
       else{
